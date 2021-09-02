d9a125dde4460204c9393f46921b2a24f22e38d4
LDSGOhGo <56556525+LDSGOhGo@users.noreply.github.com>
2021-09-02 09:12:51 -0600

    Add files via upload

---
+++ Chapter 19/Exercise19_09.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Exercise19_09 {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    list.add(14);
+    list.add(24);
+    list.add(4);
+    list.add(42);
+    list.add(5);
+    Exercise19_09.<Integer>sort(list);
+    
+    System.out.print(list);
+  }
+  public static <E extends Comparable<E>> void sort(ArrayList<E> list) {
+    E holdings;
+    for(int i = 0; i < list.size(); i++) {
+      for(int j = i + 1; j < list.size(); j++) {
+        if(list.get(i).compareTo(list.get(j)) > 0) {
+          holdings = list.get(j);
+          list.set(j, list.get(i));
+          list.set(i, holdings);
+        }
+      }
+    }
+  }
+}
